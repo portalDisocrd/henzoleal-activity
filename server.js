bb3ef02b1fde0d772599ac531e9873b36cc47853
Lucas Leal <POKPOKPK3@GMAIL.COM>
2026-08-23 18:12:24 -0300

    Preparar servidor para hospedagem

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Salas em memória
 const rooms = new Map();
